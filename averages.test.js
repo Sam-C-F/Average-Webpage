03a423b059e02223fe9f3ad6fad6946fb177d728
sam-c-f <sam_friedman@hotmail.co.uk>
2022-11-02 13:54:11 +0000

    Revert "wrote first test for averages function"
    This reverts commit 2906c414c7ae70a89d4320e367ac5e1edf8ba3f2.

--- averages.test.js
+++
@@ -1,7 +0,0 @@
-const { averageOfSeries } = require("./averages");
-
-describe("takes a series of numbers and outputs the mean, median and mode", () => {
-  it("should return the same number 3 times in an array if provided with a single number", () => {
-    expect(averageOfSeries(5)).toEqual([5, 5, 5]);
-  });
-});
