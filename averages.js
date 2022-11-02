ee6d251ab54347f56aa0508f00afffcfa4812a5d
sam-c-f <sam_friedman@hotmail.co.uk>
2022-11-02 14:47:49 +0000

    added solution for simple arrays

--- averages.js
+++ averages.js
@@ -1,3 +1,36 @@
-exports.averageOfSeries = (num) => {
-  return [num, num, num];
+exports.averageOfSeries = (...nums) => {
+  const orderedSeries = [...nums];
+
+  let totalOfSeries = 0;
+  orderedSeries.forEach((number) => (totalOfSeries += number));
+  const mean = totalOfSeries / orderedSeries.length;
+
+  let median = 0;
+  if (!orderedSeries.length % 2 === 0) {
+    median = orderedSeries[Math.floor(orderedSeries.length / 2)];
+  } else {
+    median =
+      (orderedSeries[Math.floor(orderedSeries.length / 2)] +
+        orderedSeries[Math.floor(orderedSeries.length / 2) + 1]) /
+      2;
+  }
+
+  const numberFrequency = {};
+  orderedSeries.forEach(
+    (number) => (numberFrequency[number] = numberFrequency[number] + 1 || 1)
+  );
+  let modes = [];
+  let max = 0;
+  for (const key in numberFrequency) {
+    const number = key;
+    const count = numberFrequency[key];
+    if (count > max) {
+      modes = [parseInt(number)];
+      max = count;
+    } else if (count === max) {
+      modes.push(parseInt(number));
+    }
+  }
+
+  return [mean, median, modes];
 };
--- averages.test.js
+++ averages.test.js
@@ -2,6 +2,9 @@ const { averageOfSeries } = require("./averages");
 
 describe("takes a series of numbers and outputs the mean, median and mode", () => {
   it("should return the same number 3 times in an array if provided with a single number", () => {
-    expect(averageOfSeries(5)).toEqual([5, 5, 5]);
+    expect(averageOfSeries(5)).toEqual([5, 5, [5]]);
+  });
+  it("should return [5, 5, 5] when provided with the series 4, 5, 5, 6", () => {
+    expect(averageOfSeries(4, 5, 5, 6)).toEqual([5, 5, [5]]);
   });
 });
